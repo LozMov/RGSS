c2df3cd2d79cf91db3d5ef0a46b0dbe31e3f3685
LozMov <fuzeyi98@163.com>
2016-09-15 00:57:05 +0800

    Optimize code

--- IniFile.rb
+++ IniFile.rb
@@ -1,38 +1,46 @@
 #==============================================================================
 # ■ IniFile
 #------------------------------------------------------------------------------
-# 　提供简单的INI文件读写功能。
+# 　提供简单的INI文件读写功能。依赖于转码模块。
 #------------------------------------------------------------------------------
 # 　作者：失落的乐章
 # 　来源：https://www.s-gs.net/ini_script
 #==============================================================================
 
+module INI
+  WriteString = Win32API.new('kernel32', 'WritePrivateProfileString', 'pppp', 'l')
+  GetString = Win32API.new('kernel32', 'GetPrivateProfileString', 'pppplp', 'l')
+  NULL = "\0"
+end
+
 class IniFile
+  include INI
+  attr_accessor :default, :buffer_size
+  
   def initialize(file)
     @file = file
+    @default = ""
+    @buffer_size = 128
   end
   
   def [](app)
-    IniKey.new(@file, app.to_s)
+    IniKey.new(@file, app.to_s, @default, @buffer_size)
   end
 end
 
 class IniKey
-  WriteString = Win32API.new('kernel32', 'WritePrivateProfileString', 'pppp', 'l')
-  GetString = Win32API.new('kernel32', 'GetPrivateProfileString', 'pppplp', 'l')
-  
-  def initialize(file, app)
-    @file = file
-    @app = app
+  include INI
+  def initialize(file, app, default, size)
+    @file, @app, @default, @size = file, app, default, size
   end
   
-  def [](key, default = "", size = 64)
-    buff = "\0" * size
-    GetString.call(@app, key.to_s, default, buff, size, @file)
-    return buff.delete("\0")
+  def [](key)
+    buf = NULL * @size
+    GetString.call(@app.u2s, key.to_s.u2s, @default, buf, @size, @file)
+    return buf.delete(NULL).s2u
   end
   
   def []=(key, value)
-    WriteString.call(@app, key.to_s, value.to_s, @file)
+    WriteString.call(@app.u2s, key.to_s.u2s, value.to_s.u2s, @file)
   end
 end
