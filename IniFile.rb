7a019008de75d0170debf376271d195272350f9d
LozMov <fuzeyi98@163.com>
2016-10-15 22:23:59 +0800

    Optimize code

--- IniFile.rb
+++ IniFile.rb
@@ -7,14 +7,10 @@
 # 　来源：https://www.s-gs.net/ini_script
 #==============================================================================
 
-module INI
+class IniFile
   WriteString = Win32API.new('kernel32', 'WritePrivateProfileString', 'pppp', 'l')
   GetString = Win32API.new('kernel32', 'GetPrivateProfileString', 'pppplp', 'l')
-  NULL = "\0"
-end
-
-class IniFile
-  include INI
+  
   attr_accessor :default, :buffer_size
   
   def initialize(file)
@@ -26,21 +22,20 @@ class IniFile
   def [](app)
     IniKey.new(@file, app.to_s, @default, @buffer_size)
   end
-end
 
-class IniKey
-  include INI
-  def initialize(file, app, default, size)
-    @file, @app, @default, @size = file, app, default, size
-  end
+  class IniKey
+    def initialize(file, app, default, size)
+      @file, @app, @default, @size = file, app, default, size
+    end
   
-  def [](key)
-    buf = NULL * @size
-    GetString.call(@app.u2s, key.to_s.u2s, @default, buf, @size, @file)
-    return buf.delete(NULL).s2u
-  end
+    def [](key)
+      buf = "\0" * @size
+      GetString.call(@app.u2s, key.to_s.u2s, @default, buf, @size, @file)
+      return buf.delete("\0").s2u
+    end
   
-  def []=(key, value)
-    WriteString.call(@app.u2s, key.to_s.u2s, value.to_s.u2s, @file)
+    def []=(key, value)
+      WriteString.call(@app.u2s, key.to_s.u2s, value.to_s.u2s, @file)
+    end
   end
 end
